6872c0a408fb06ae8034ccfc16efa8bf2492c0e2
WinjayYu <winjayyu@gmial.com>
2016-11-28 09:14:09 +0800

    1.规定ResultStatus类里面的三个字段(Not_Login,Error,Success)的值及代表的意思 2.完善Hosue的entity类 3.写了UserApi里面的getorder接口

--- src/main/java/com/ryel/zaja/config/bean/ResultStatus.java
+++ src/main/java/com/ryel/zaja/config/bean/ResultStatus.java
@@ -4,12 +4,14 @@ package com.ryel.zaja.config.bean;
  * Author: koabs
  * 8/22/16.
  */
+
+//三种状态，1表示error, 0表示成功，-1表示用户未登录
 public class ResultStatus {
     // 用户未登入
     public static Integer Not_Login = -1;
 
-    public static Integer Error = 500;
+    public static Integer Error = 1;
 
-    public static Integer Success = 200;
+    public static Integer Success = 0;
 
 }
