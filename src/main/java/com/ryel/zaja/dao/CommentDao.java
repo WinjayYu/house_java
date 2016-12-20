6a48c1ce08d80d1790fa755766f49213175b79ff
WinjayYu <winjayyu@gmial.com>
2016-12-20 09:44:18 +0800

    提交代码

--- src/main/java/com/ryel/zaja/dao/CommentDao.java
+++ src/main/java/com/ryel/zaja/dao/CommentDao.java
@@ -1,11 +1,6 @@
 package com.ryel.zaja.dao;
 
 import com.ryel.zaja.entity.Comment;
-<<<<<<< HEAD
-=======
-import com.ryel.zaja.entity.House;
-import com.ryel.zaja.entity.User;
->>>>>>> origin/master
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 /**
  * Created by billyu on 2016/12/19.
  */
@@ -25,13 +19,4 @@ public interface CommentDao extends JpaRepository<Comment, Integer>,JpaSpecifica
 
     @Query("select c from Comment c where c.agent.id = ?1")
     Page<Comment> findByAgentId(Integer anentId, Pageable pageable);
-=======
-import java.util.List;
-
-
-@Repository
-public interface CommentDao extends JpaRepository<Comment, Integer>,JpaSpecificationExecutor<Comment> {
-    @Query("select u from Comment u where u.agent.id = ?1")
-    Page<Comment> pageByAgentId(Integer agentId, Pageable pageable);
->>>>>>> origin/master
 }
