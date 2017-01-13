3a34658f41d20fe02df9d27f80c6aec2d419828e
WinjayYu <winjayyu@gmial.com>
2017-01-13 17:40:15 +0800

    修改bug

--- src/main/java/com/ryel/zaja/dao/TradeRecordDao.java
+++ src/main/java/com/ryel/zaja/dao/TradeRecordDao.java
@@ -3,11 +3,14 @@ package com.ryel.zaja.dao;
 import com.ryel.zaja.entity.TradeRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TradeRecordDao extends JpaRepository<TradeRecord, Integer> ,JpaSpecificationExecutor<TradeRecord> {
+
+    @Query("select t from TradeRecord t")
     List<TradeRecord> findByThirdHtId(String thirdHtId);
 }
