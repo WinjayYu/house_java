0d799bb6e86356e7b51e27acc214b102dfd6d48e
WinjayYu <winjayyu@gmial.com>
2017-01-18 18:57:50 +0800

    修改拦截器bug。添加功能：用户支付成功后，对应的房屋所有订单进入关闭状态

--- src/main/java/com/ryel/zaja/config/MyWebAppConfig.java
+++ src/main/java/com/ryel/zaja/config/MyWebAppConfig.java
@@ -16,7 +16,6 @@ public class MyWebAppConfig extends WebMvcConfigurerAdapter {
         // 多个拦截器组成一个拦截器链
         // addPathPatterns 用于添加拦截规则
         // excludePathPatterns 用户排除拦截
-
         registry.addInterceptor(new MyInterceptor())
                 .addPathPatterns("/**/api/**");
         super.addInterceptors(registry);
