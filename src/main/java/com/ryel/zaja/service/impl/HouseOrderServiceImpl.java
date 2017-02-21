ebcdce5fdf08f7f1ed7a2f6b42db608ebee43359
WinjayYu <winjayyu@gmial.com>
2017-02-21 11:40:17 +0800

    提交代码

--- src/main/java/com/ryel/zaja/service/impl/HouseOrderServiceImpl.java
+++ src/main/java/com/ryel/zaja/service/impl/HouseOrderServiceImpl.java
@@ -143,7 +143,8 @@ public class HouseOrderServiceImpl extends AbsCommonService<HouseOrder> implemen
         List<HouseOrder> houseOrders = houseOrderDao.findAllOrderByHouseId(houseOrder.getHouse().getId());
         for(HouseOrder houseOrder2: houseOrders){
             if(HouseOrderStatus.getPayedList().contains(houseOrder2.getStatus())){
-                throw new BizException(Error_code.ERROR_CODE_0026,"订单存在已支付的订单");
+                houseOrder.setStatus(HouseOrderStatus.CLOSED.getCode());
+                throw new BizException(Error_code.ERROR_CODE_0026,"房源存在已支付的订单");
             }
         }
         if(!HouseOrderStatus.WAIT_PAYMENT.getCode().equals(houseOrder.getStatus())){
@@ -161,13 +162,13 @@ public class HouseOrderServiceImpl extends AbsCommonService<HouseOrder> implemen
                 }
 
             }
-
+/*
             //用户支付成功，对应的房屋所有其他的订单进入关闭状态
             houseOrders.remove(houseOrder);
             for(HouseOrder houseOrder1 : houseOrders){
                 houseOrder1.setStatus(HouseOrderStatus.CLOSED.getCode());
                 update(houseOrder);
-            }
+            }*/
         }
         return update(houseOrder);
     }
